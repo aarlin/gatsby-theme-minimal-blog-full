6a5fa46c2c7b6b4107a3afa856062b7fca123ede
Aaron Lin <aaronlin256@gmail.com>
2020-02-08 17:21:14 -0500

    refactor(files): Remove unnecessary files

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -2,10 +2,6 @@
 import { jsx } from "theme-ui"
 import { Link } from "gatsby"
 import Layout from "./layout"
-// @ts-ignore
-import Hero from "../texts/hero"
-// @ts-ignore
-import Bottom from "../texts/bottom"
 import Title from "./title"
 import Listing from "./listing"
 import List from "./list"
@@ -29,16 +25,10 @@ const Homepage = ({ posts }: PostsProps) => {
 
   return (
     <Layout>
-      <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
-        <Hero />
-      </section>
       <Title text="Latest Posts">
         <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Read all posts</Link>
       </Title>
       <Listing posts={posts} showTags={false} />
-      <List>
-        <Bottom />
-      </List>
     </Layout>
   )
 }
