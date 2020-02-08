0c4072c5cfaafa24ec6db1371888c1e4c2b5576b
Aaron Lin <aaronlin256@gmail.com>
2020-02-08 14:47:22 -0500

    refactor(minimal-blog): Start point for minimal blog with full control of all content

--- themes/gatsby-theme-minimal-blog-core/gatsby-config.js
+++ themes/gatsby-theme-minimal-blog-core/gatsby-config.js
@@ -1,49 +1,53 @@
 const withDefaults = require(`./utils/default-options`)
 
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: withDefaults.postsPath,
-        path: withDefaults.postsPath,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: withDefaults.pagesPath,
-        path: withDefaults.pagesPath,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 960,
-              quality: 90,
-              linkImagesToOriginal: false,
-            },
-          },
-        ],
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 960,
-              quality: 90,
-              linkImagesToOriginal: false,
-            },
-          },
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-typescript`,
-  ]
-}
+module.exports = themeOptions => {
+  const options = withDefaults(themeOptions)
+  const { mdx = true } = themeOptions
 
+  return {
+    plugins: [
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: options.postsPath,
+          path: options.postsPath,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: options.pagesPath,
+          path: options.pagesPath,
+        },
+      },
+      mdx && {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          gatsbyRemarkPlugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 960,
+                quality: 90,
+                linkImagesToOriginal: false,
+              },
+            },
+          ],
+          plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 960,
+                quality: 90,
+                linkImagesToOriginal: false,
+              },
+            },
+          ],
+        },
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-typescript`,
+    ].filter(Boolean),
+  }
+}
