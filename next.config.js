d748f1a23fc06cc009022ccc8d6606cd8dcafb47
Aaron Lin <aaronlin256@gmail.com>
2023-02-20 16:49:40 -0500

    Move to latest LTS node

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
 const moduleExports = {
   swcMinify: true,
-  images: {
-    domains: [
-      'img.youtube.com',
-      'dl.airtable.com',
-      'v5.airtableusercontent.com',
-      'opengraph.githubassets.com',
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/twitch',
-        destination: 'https://twitch.tv/',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = moduleExports;
